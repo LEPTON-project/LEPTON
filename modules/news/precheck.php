cc2f8f67c0379ba82ce54bf42f2c28fcaa9dca51
erpe <rp@cms-lab.com>
2012-11-28 17:21:00 +0100

    add precheck to news module

---
+++ modules/news/precheck.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *  @module         news
+ *  @version        see info.php of this module
+ *  @author         Ryan Djurovich, Rob Smith, Dietrich Roland Pehlke, Christian M. Stefan (Stefek), Jurgen Nijhuis (Argos)
+ *  @copyright      2004-2012 Ryan Djurovich, Rob Smith, Dietrich Roland Pehlke, Christian M. Stefan (Stefek), Jurgen Nijhuis (Argos) 
+ *  @license        GNU General Public License
+ *  @license terms  see info.php of this module
+ *  @platform       see info.php of this module
+ *  @requirements   PHP 5.2.x and higher
+ * 
+ */
+
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {	
+	include(WB_PATH.'/framework/class.secure.php'); 
+} elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php')) {
+	include($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php'); 
+} else {
+	$subs = explode('/', dirname($_SERVER['SCRIPT_NAME']));	$dir = $_SERVER['DOCUMENT_ROOT'];
+	$inc = false;
+	foreach ($subs as $sub) {
+		if (empty($sub)) continue; $dir .= '/'.$sub;
+		if (file_exists($dir.'/framework/class.secure.php')) { 
+			include($dir.'/framework/class.secure.php'); $inc = true;	break; 
+		} 
+	}
+	if (!$inc) trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+}
+// end include class.secure.php
+
+// Checking Requirements
+$PRECHECK['LEPTON_VERSION'] = array('VERSION' => '1.2.1', 'OPERATOR' => '>=');
+
+?>
