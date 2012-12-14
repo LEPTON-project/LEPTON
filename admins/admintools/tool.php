cce8006e182e0c94999132189a775690b06d8647
erpe <mailus@lepton-cms.org>
2012-12-14 10:36:32 +0100

    modification needed for dropleps

--- admins/admintools/tool.php
+++ admins/admintools/tool.php
@@ -9,32 +9,38 @@
  *
  * @author          Website Baker Project, LEPTON Project
  * @copyright       2004-2010, Website Baker Project
- * @copyright       2010-2011, LEPTON Project
+ * @copyright       2010-2012 LEPTON Project
  * @link            http://www.LEPTON-cms.org
  * @license         http://www.gnu.org/licenses/gpl.html
- * @license_terms   please see LICENSE and COPYING files in your package
- * @version         $Id: tool.php 1172 2011-10-04 15:26:26Z frankh $
  *
  */
  
 // include class.secure.php to protect this file and the whole CMS!
-if (defined('WB_PATH')) {	
-	include(WB_PATH.'/framework/class.secure.php'); 
-} else {
-	$oneback = "../";
-	$root = $oneback;
-	$level = 1;
-	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
-		$root .= $oneback;
-		$level += 1;
-	}
-	if (file_exists($root.'/framework/class.secure.php')) { 
-		include($root.'/framework/class.secure.php'); 
-	} else {
-		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
-	}
+if ( defined( 'WB_PATH' ) )
+{
+    include( WB_PATH . '/framework/class.secure.php' );
+}
+else
+{
+    $root  = "../";
+    $level = 1;
+    while ( ( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) ) )
+    {
+        $root .= "../";
+        $level += 1;
+    }
+    if ( file_exists( $root . '/framework/class.secure.php' ) )
+    {
+        include( $root . '/framework/class.secure.php' );
+    }
+    else
+    {
+        trigger_error( sprintf( "[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER[ 'SCRIPT_NAME' ] ), E_USER_ERROR );
+    }
 }
 // end include class.secure.php
+
+
 
 require_once(WB_PATH.'/framework/class.admin.php');
 require_once(WB_PATH.'/framework/functions.php');
@@ -47,28 +53,19 @@ if(!isset($_GET['tool'])) {
 }
 
 // Check if tool is installed
-$fields = array( 'name', 'directory');
-$query = $database->build_mysql_query(
-	"SELECT",
-	TABLE_PREFIX."addons",
-	$fields,
-	"`type` = 'module' AND `function` = 'tool' AND `directory` = '".$admin->add_slashes($_GET['tool'])."'"
-);
-
-$result = $database->query( $query );
-
+$result = $database->query("SELECT * FROM ".TABLE_PREFIX."addons WHERE type = 'module' AND function = 'tool' AND directory = '".$admin->add_slashes($_GET['tool'])."'");
 if($result->numRows() == 0) {
 	header("Location: index.php");
 	exit(0);
 }
-$tool = $result->fetchRow( MYSQL_ASSOC );
+$tool = $result->fetchRow();
 
 ?>
-<div class="container">
-
-<h2>
+<h4>
+	<a href="<?php echo ADMIN_URL; ?>/admintools/index.php"><?php echo $HEADING['ADMINISTRATION_TOOLS']; ?></a>
+	&raquo;
 	<?php echo $tool['name']; ?>
-</h2>
+</h4>
 <?php
 
 if(file_exists(WB_PATH.'/modules/'.$tool['directory'].'/tool.php'))
@@ -78,4 +75,6 @@ if(file_exists(WB_PATH.'/modules/'.$tool['directory'].'/tool.php'))
 } else {
 	$admin->print_error($MESSAGE['GENERIC_ERROR_OPENING_FILE'] );
 }
+
+
 ?>
