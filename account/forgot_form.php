9e4ca0d63f6dbc2ac2f1f5df5ccd6de59ce95561
Dietrich <dietrich@powermac-g4.speedport.ip>
2013-03-18 10:44:07 +0100

    minor cosmetic changes inside account/forgot_form.php

--- account/forgot_form.php
+++ account/forgot_form.php
@@ -142,55 +142,57 @@ $tpl->set_unknowns('remove');
 
 // see if there exists a frontend template file or use the fallback
 if (file_exists(WB_PATH.'/templates/'.DEFAULT_TEMPLATE.'/frontend_login/forgot_form.php')) 
-  {
-      require_once(WB_PATH.'/templates/'.DEFAULT_TEMPLATE.'/frontend_login/forgot_form.php');
-  }
-      else {
-/**
- *	set template file name
- *
- */
-$tpl->set_file('forgot', 'forgot_form.htt');
-
-/**
- *
- *
- */
-$hash = sha1( microtime().$_SERVER['HTTP_USER_AGENT'] );
-$_SESSION['wb_apf_hash'] = $hash;
-
-$redirect_url = (isset($_SESSION['HTTP_REFERER']) ? $_SESSION['HTTP_REFERER'] : "");
-
-$tpl->set_var(array(
-	'TEMPLATE_DIR' 		=>	TEMPLATE_DIR,
-	'WB_URL'			=>	WB_URL,
-	'URL'			    =>	$redirect_url,
-	'FORGOT_URL'		=>	FORGOT_URL,  
-	'TEXT_FORGOT'		=>	$MENU['FORGOT'],  
-	'MESSAGE_COLOR'		=>	$message_color,
-	'MESSAGE'		    =>	$message,  
-	'TEXT_EMAIL'		=>	$TEXT['EMAIL'],
-	'DISPLAY_EMAIL'		=>	$email,   
-	'TEXT_SEND_DETAILS'	=>	$TEXT['SEND_DETAILS'],
-	'TEXT_LOGOUT'		=>	$MENU['LOGOUT'],
-	'TEXT_RESET'		=>	$TEXT['RESET'],
-	'HASH'				=>	$hash,
-	'TEXT_FORGOTTEN_DETAILS' => $TEXT['FORGOTTEN_DETAILS']
-	)
-);
-
-unset($_SESSION['result_message']);
-
-$tpl->set_block('forgot', 'comment_block', 'comment_replace'); 
-$tpl->set_block('comment_replace', '');
-
-if(!isset($display_form) OR $display_form != false) {
-
-} else {
-	$tpl->set_block('forgot', 'form_block', 'form_block_ref');
-	$tpl->set_block('form_block_ref', '');
+{
+	require_once(WB_PATH.'/templates/'.DEFAULT_TEMPLATE.'/frontend_login/forgot_form.php');
 }
-// ouput the final template
-$tpl->pparse('output', 'forgot');  
+else
+{
+	/**
+	 *	set template file name
+	 *
+	 */
+	$tpl->set_file('forgot', 'forgot_form.htt');
+	
+	/**
+	 *	Build a secure hash.
+	 *
+	 */
+	$hash = sha1( microtime().$_SERVER['HTTP_USER_AGENT'] );
+	$_SESSION['wb_apf_hash'] = $hash;
+	
+	$redirect_url = (isset($_SESSION['HTTP_REFERER']) ? $_SESSION['HTTP_REFERER'] : "");
+	
+	$tpl->set_var(array(
+		'TEMPLATE_DIR' 		=>	TEMPLATE_DIR,
+		'WB_URL'			=>	WB_URL,
+		'URL'			    =>	$redirect_url,
+		'FORGOT_URL'		=>	FORGOT_URL,  
+		'TEXT_FORGOT'		=>	$MENU['FORGOT'],  
+		'MESSAGE_COLOR'		=>	$message_color,
+		'MESSAGE'		    =>	$message,  
+		'TEXT_EMAIL'		=>	$TEXT['EMAIL'],
+		'DISPLAY_EMAIL'		=>	$email,   
+		'TEXT_SEND_DETAILS'	=>	$TEXT['SEND_DETAILS'],
+		'TEXT_LOGOUT'		=>	$MENU['LOGOUT'],
+		'TEXT_RESET'		=>	$TEXT['RESET'],
+		'HASH'				=>	$hash,
+		'TEXT_FORGOTTEN_DETAILS' => $TEXT['FORGOTTEN_DETAILS']
+		)
+	);
+	
+	unset($_SESSION['result_message']);
+	
+	$tpl->set_block('forgot', 'comment_block', 'comment_replace'); 
+	$tpl->set_block('comment_replace', '');
+	
+	if(!isset($display_form) OR $display_form != false)
+	{
+		// nothing
+	} else {
+		$tpl->set_block('forgot', 'form_block', 'form_block_ref');
+		$tpl->set_block('form_block_ref', '');
+	}
+	// ouput the final template
+	$tpl->pparse('output', 'forgot');  
 }
 ?>
