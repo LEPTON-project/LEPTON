601586b707fb8081a880606f95ff193998213b46
erpe <mailus@lepton-cms.org>
2013-03-21 09:15:14 +0100

    cosmetic change

--- install/support.php
+++ install/support.php
@@ -22,7 +22,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>LEPTON Update Script</title>
+<title>LEPTON installation Script</title>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <link href="http://lepton-cms.org/_packinstall/update.css" rel="stylesheet" type="text/css" />
 </head>
