1137ba72c4470cfa75f512bdf1c9dd65920451dd
erpe <mailus@lepton-cms.org>
2014-01-27 12:57:03 +0100

    upgrade jquery to 1.11 http://blog.jquery.com/2014/01/24/jquery-1-11-and-2-1-released/

--- upload/modules/lib_jquery/jquery-core/index.php
+++ upload/modules/lib_jquery/jquery-core/index.php
@@ -5,37 +5,30 @@
  * This ADDON is released under the GNU GPL.
  * Additional license terms can be seen in the info.php of this module.
  *
- * @module          lib_jQuery
+ *
  * @author          LEPTON Project
- * @copyright       2010-2013 LEPTON Project
- * @link            http://www.lepton-cms.org
+ * @copyright       2010-2014 LEPTON Project
+ * @link            http://www.LEPTON-cms.org
  * @license         http://www.gnu.org/licenses/gpl.html
  * @license_terms   please see info.php of this module
  *
  */
 
 // include class.secure.php to protect this file and the whole CMS!
-if ( defined( 'LEPTON_PATH' ) )
-{
-	include( LEPTON_PATH . '/framework/class.secure.php' );
-} //defined( 'LEPTON_PATH' )
-else
-{
+if (defined('WB_PATH')) {	
+	include(WB_PATH.'/framework/class.secure.php'); 
+} else {
 	$oneback = "../";
-	$root    = $oneback;
-	$level   = 1;
-	while ( ( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) ) )
-	{
+	$root = $oneback;
+	$level = 1;
+	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
 		$root .= $oneback;
 		$level += 1;
-	} //( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) )
-	if ( file_exists( $root . '/framework/class.secure.php' ) )
-	{
-		include( $root . '/framework/class.secure.php' );
-	} //file_exists( $root . '/framework/class.secure.php' )
-	else
-	{
-		trigger_error( sprintf( "[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER[ 'SCRIPT_NAME' ] ), E_USER_ERROR );
+	}
+	if (file_exists($root.'/framework/class.secure.php')) { 
+		include($root.'/framework/class.secure.php'); 
+	} else {
+		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
 	}
 }
 // end include class.secure.php
