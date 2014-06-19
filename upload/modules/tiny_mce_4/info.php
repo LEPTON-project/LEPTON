0610b3eb9c86ddba25ffe2c1b35384c429bea1a6
erpe <rp@cms-lab.com>
2014-06-19 09:18:30 +0200

    update tiny_mce to 4.1.0 http://www.tinymce.com/develop/changelog/index.php?type=tinymce

--- upload/modules/tiny_mce_4/info.php
+++ upload/modules/tiny_mce_4/info.php
@@ -36,18 +36,20 @@ if (defined('LEPTON_PATH')) {
 $module_directory     = 'tiny_mce_4';
 $module_name          = 'TinyMCE-4-jq';
 $module_function      = 'WYSIWYG';
-$module_version       = '4.0.27.0';
+$module_version       = '4.1.0.0';
 $module_platform      = '2.x';
 $module_author        = 'erpe, Aldus';
 $module_home          = 'http://lepton-cms.org';
 $module_guid          = '0ad7e8dd-2f6b-4525-b4bf-db326b0f5ae8';
 $module_license       = 'GNU General Public License, TINYMCE is LGPL, Ajax Filemanager is also open source license.';
 $module_license_terms  = '-';
-$module_description   = 'TinyMCE 4.0.27 - build date:(2014-05-27)<br>with Ajax Image File Manager and image editor<BR> allows you to edit the content of a page and see media image folder.<BR>To link your template css file to the styles in tinymce you need to edit the <b>include.php</b> file inside this module.';
+$module_description   = 'TinyMCE 4.1.0 - build date:(2014-06-18)<br>with Ajax Image File Manager and image editor<BR> allows you to edit the content of a page and see media image folder.<BR>To link your template css file to the styles in tinymce you need to edit the <b>include.php</b> file inside this module.';
 
 /**
- *	2014-05-27	- update to 4.0.27
+ *	2014-06-18	- update to 4.1.0
  *
+ *	2014-05-27	- update to 4.0.27
+ * 
  *	2014-04-30	- update to 4.0.24
  * 
  *	2014-04-23	- update to 4.0.23
