e08a317304c0fec08e7d2d3fb3a5a4a1eb21fb97
aldus <info@drp-x-work.de>
2014-09-13 22:04:42 +0200

    Codechanges inside news/add_post.php for PDO.

--- upload/modules/news/add_post.php
+++ upload/modules/news/add_post.php
@@ -44,25 +44,45 @@ $order = new order(TABLE_PREFIX.'mod_news_posts', 'position', 'post_id', 'sectio
 $position = $order->get_new($section_id);
 
 // Get default commenting
-$query_settings = $database->query("SELECT commenting FROM ".TABLE_PREFIX."mod_news_settings WHERE section_id = '$section_id'");
-$fetch_settings = $query_settings->fetchRow();
+$fetch_settings = array();
+$database->prepare_and_execute(
+	"SELECT commenting FROM ".TABLE_PREFIX."mod_news_settings WHERE section_id = '".$section_id."'",
+	true,
+	$fetch_settings,
+	false
+);
 $commenting = $fetch_settings['commenting'];
 
 // Insert new row into database
-$database->query("INSERT INTO ".TABLE_PREFIX."mod_news_posts (`section_id`, `page_id`, `position`, `commenting`, `active`, `link`, `content_short`, `content_long`) VALUES ('$section_id', '$page_id', '$position', '$commenting', '1', '', '', '')");
+$fields = array(
+	'section_id'	=> $section_id,
+	'page_id'		=> $page_id,
+	'position'		=> $position,
+	'commenting'	=> $commenting,
+	'active'		=> 1,
+	'link'			=> "",
+	'content_short'	=> "",
+	'content_long'	=> ""
+	
+);
+
+$database->build_and_execute(
+	"insert",
+	TABLE_PREFIX."mod_news_posts",
+	$fields
+);
 
 // Get the id
-$post_id = $database->get_one("SELECT LAST_INSERT_ID()");
+$post_id = $database->db_handle->lastInsertId();
 
 // Say that a new record has been added, then redirect to modify page
 if($database->is_error()) {
 	$admin->print_error($database->get_error(), WB_URL.'/modules/news/modify_post.php?page_id='.$page_id.'&section_id='.$section_id.'&post_id='.$post_id);
 } else {
-	//$admin->print_success($TEXT['SUCCESS'], WB_URL.'/modules/news/modify_post.php?page_id='.$page_id.'&section_id='.$section_id.'&post_id='.$post_id);
 	?>
 <script type="text/javascript">
 		setTimeout("top.location.href ='<?php echo WB_URL; ?>/modules/news/modify_post.php?page_id=<?php echo $page_id; ?>&section_id=<?php echo $section_id; ?>&post_id=<?php echo $post_id; ?>'", 0);
-	</script>
+</script>
 <?php
 }
 
