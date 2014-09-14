1299cb042afc70f22c1c8fa01832fbdf73c8be6b
aldus <info@drp-x-work.de>
2014-09-14 15:38:55 +0200

    Codechanges inside news/modify_comment.php for Twig. Also add backend-theme-template to algos.

--- upload/modules/news/modify_comment.php
+++ upload/modules/news/modify_comment.php
@@ -31,8 +31,6 @@ if (defined('LEPTON_PATH')) {
 }
 // end include class.secure.php
 
-
-
 // Get id
 if(!isset($_GET['comment_id']) OR !is_numeric($_GET['comment_id'])) {
 	header("Location: ".ADMIN_URL."/pages/index.php");
@@ -44,49 +42,57 @@ if(!isset($_GET['comment_id']) OR !is_numeric($_GET['comment_id'])) {
 // Include WB admin wrapper script
 require(LEPTON_PATH.'/modules/admin.php');
 
+/**	*******************************
+ *	Try to get the template-engine.
+ */
+global $parser, $loader;
+if (!isset($parser))
+{
+	require_once( LEPTON_PATH."/modules/lib_twig/library.php" );
+}
+
+$loader->prependPath( dirname(__FILE__)."/templates/backend/", "news" );
+
+$backend_template_path = LEPTON_PATH."/templates/".DEFAULT_THEME."/backend/news/";
+$module_template_path = dirname(__FILE__)."/templates/backend/";
+
+require_once (LEPTON_PATH."/modules/lib_twig/classes/class.twig_utilities.php");
+$twig_util = new twig_utilities( $parser, $loader, $module_template_path, $backend_template_path );
+$twig_util->template_namespace = "news";
+
+// end - template
+
 // Get header and footer
-$query_content = $database->query("SELECT post_id,title,comment FROM ".TABLE_PREFIX."mod_news_comments WHERE comment_id = '$comment_id'");
-$fetch_content = $query_content->fetchRow();
+$fetch_content = array();
+$values = array($comment_id);
+$query_content = $database->prepare_and_execute(
+	"SELECT `post_id`,`title`,`comment` FROM `".TABLE_PREFIX."mod_news_comments` WHERE comment_id = ?",
+	$values,
+	true,
+	$fetch_content,
+	false
+);
 
-?>
+$form_values = array(
+	'TEXT_MODIFY'	=> $TEXT['MODIFY'],
+	'LEPTON_URL'	=> LEPTON_URL,
+	'section_id'	=> $section_id,
+	'page_id'		=> $page_id,
+	'post_id'		=> $fetch_content['post_id'],
+	'comment_id'	=> $comment_id,
+	'TEXT_TITLE'	=> $TEXT['TITLE'],
+	'title'			=> htmlspecialchars($fetch_content['title']),
+	'TEXT_COMMENT'	=> $TEXT['COMMENT'],
+	'comment'		=> htmlspecialchars($fetch_content['comment']),
+	'SAVE'			=> $TEXT['SAVE'],
+	'CANCEL'		=> $TEXT['CANCEL']
+);
 
-<h2><?php echo $TEXT['MODIFY'].' '.$TEXT['COMMENT']; ?></h2>
-
-<form name="modify" action="<?php echo LEPTON_URL; ?>/modules/news/save_comment.php" method="post" style="margin: 0;">
-
-<input type="hidden" name="section_id" value="<?php echo $section_id; ?>" />
-<input type="hidden" name="page_id" value="<?php echo $page_id; ?>" />
-<input type="hidden" name="post_id" value="<?php echo $fetch_content['post_id']; ?>" />
-<input type="hidden" name="comment_id" value="<?php echo $comment_id; ?>" />
-
-<table class="row_a" cellpadding="2" cellspacing="0" border="0" width="100%">
-<tr>
-	<td width="80"><?php echo $TEXT['TITLE']; ?>:</td>
-	<td>
-		<input type="text" name="title" value="<?php echo (htmlspecialchars($fetch_content['title'])); ?>" style="width: 98%;" maxlength="255" />
-	</td>
-</tr>
-<tr>
-	<td valign="top"><?php echo $TEXT['COMMENT']; ?>:</td>
-	<td>
-		<textarea name="comment" rows="10" cols="1" style="width: 98%; height: 150px;"><?php echo (htmlspecialchars($fetch_content['comment'])); ?></textarea>
-	</td>
-</tr>
-</table>
-
-<table cellpadding="0" cellspacing="0" border="0" width="100%">
-<tr>
-	<td align="left">
-		<input name="save" type="submit" value="<?php echo $TEXT['SAVE']; ?>" style="width: 100px; margin-top: 5px;" />
-	</td>
-	<td align="right">
-		<input type="button" value="<?php echo $TEXT['CANCEL']; ?>" onclick="javascript: window.location = '<?php echo LEPTON_URL; ?>/modules/news/modify_post.php?page_id=<?php echo $page_id; ?>&amp;section_id=<?php echo $section_id; ?>&amp;post_id=<?php echo $fetch_content['post_id']; ?>';" style="width: 100px; margin-top: 5px;" />
-	</td>
-</tr>
-</table>
-</form>
-
-<?php
+$twig_util->resolve_path("modify_comment.lte");
+echo $parser->render(
+	'@news/modify_comment.lte',
+	$form_values
+);
 
 // Print admin footer
 $admin->print_footer();
