f6104cf06c25e963f39a60b853e3fc389e8e0cac
erpe <rp@cms-lab.com>
2014-09-26 09:54:07 +0200

    modify headers

--- upload/modules/news/register_parser.php
+++ upload/modules/news/register_parser.php
@@ -2,9 +2,8 @@
 /**
  *  @module         news
  *  @version        see info.php of this module
- *  @author         Ryan Djurovich, Rob Smith, Dietrich Roland Pehlke, Christian M. Stefan (Stefek), Jurgen Nijhuis (Argos), LEPTON Project
- *  @copyright      2004-2010 Ryan Djurovich, Rob Smith, Dietrich Roland Pehlke, Christian M. Stefan (Stefek), Jurgen Nijhuis (Argos) 
- * 	@copyright      2010-2014 LEPTON Project 
+ *  @author         LEPTON Project
+ * 	@copyright      2013-2014 LEPTON Project 
  *  @license        GNU General Public License
  *  @license terms  see info.php of this module
  *  @platform       see info.php of this module
