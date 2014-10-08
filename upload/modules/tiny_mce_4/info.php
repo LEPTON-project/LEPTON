da4c96acd758794f87f5e8ce3ce780254066a4fd
erpe <rp@cms-lab.com>
2014-10-08 14:47:00 +0200

    update tiny_mce to 4.1.6 http://www.tinymce.com/develop/changelog/index.php?type=tinymce

--- upload/modules/tiny_mce_4/info.php
+++ upload/modules/tiny_mce_4/info.php
@@ -36,65 +36,18 @@ if (defined('LEPTON_PATH')) {
 $module_directory     = 'tiny_mce_4';
 $module_name          = 'TinyMCE-4-jq';
 $module_function      = 'WYSIWYG';
-$module_version       = '4.1.5.0';
+$module_version       = '4.1.6.0';
 $module_platform      = '2.x';
 $module_author        = 'erpe, Aldus';
 $module_home          = 'http://lepton-cms.org';
 $module_guid          = '0ad7e8dd-2f6b-4525-b4bf-db326b0f5ae8';
 $module_license       = 'GNU General Public License, TINYMCE is LGPL, Ajax Filemanager is also open source license.';
 $module_license_terms  = '-';
-$module_description   = 'TinyMCE 4.1.5 - build date:(2014-09-09)<br>with Ajax Image File Manager and image editor<BR> allows you to edit the content of a page and see media image folder.<BR>To link your template css file to the styles in tinymce you need to edit the <b>include.php</b> file inside this module.';
+$module_description   = 'TinyMCE 4.1.6 - build date:(2014-10-08)<br>with Ajax Image File Manager and image editor<BR> allows you to edit the content of a page and see media image folder.<BR>To link your template css file to the styles in tinymce you need to edit the <b>include.php</b> file inside this module.';
 
 /**
- *	2014-09-09	- update to 4.1.5
- *
- *	2014-08-21	- update to 4.1.4
- * 
- *	2014-07-29	- update to 4.1.3
- * 
- *	2014-07-15	- update to 4.1.2
- * 
- *	2014-07-07	- update to 4.1.1
- * 
- *	2014-06-18	- update to 4.1.0
- * 
- *	2014-05-27	- update to 4.0.27
- * 
- *	2014-04-30	- update to 4.0.24
- * 
- *	2014-04-23	- update to 4.0.23
- *
- *	2014-04-16	- update to 4.0.22
- *
- *	2014-04-01	- update to 4.0.21
- * 
- *	2014-03-18	- update to 4.0.20
- *
- *	2014-03-11	- update to 4.0.19
- *
-  *	2014-02-26	- update to 4.0.17
- *
- *	2014-01-30	- update to 4.0.13
- * 
- *	2013-12-18	- update to 4.0.12
- *
- *	2013-11-20	- update to 4.0.11
- *
- *	2013-10-28	- update to 4.0.10
- * 
- *	2013-10-24	- update to 4.0.9
- * 
- *	2013-10-10	- update to 4.0.8
- *
- *	2013-10-02	- update to 4.0.7
- * 
- *	2013-09-12	- update to 4.0.6
- * 
- *	2013-08-27	- update to 4.0.5
- * 
- *	2013-08-21	- update to 4.0.4
- * 
- *	2013-08-08	- update to 4.0.3
+ * changelog details on https://github.com/LEPTON-project/LEPTON_2
+ * and for tiny_mce only on http://www.tinymce.com/develop/changelog/index.php?type=tinymce
  * 
  *	2013-07-18	- update to 4.0.2
  * 
