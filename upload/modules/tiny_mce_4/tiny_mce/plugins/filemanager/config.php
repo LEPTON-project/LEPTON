14d98d070f41526cfb1a71c67257905c862a90fe
aldus <info@drp-x-work.de>
2014-10-24 13:24:13 +0200

    smal bugfix inside TinyMCE filemanager/config.php

--- upload/modules/tiny_mce_4/tiny_mce/plugins/filemanager/config.php
+++ upload/modules/tiny_mce_4/tiny_mce/plugins/filemanager/config.php
@@ -1,7 +1,26 @@
 <?php 
 if($_SESSION["verify"] != "FileManager4TinyMCE") die('forbidden');
 
-@require_once("../../../../../config.php");
+define( 'SESSION_STARTED', true );
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('LEPTON_PATH')) {   
+   include(LEPTON_PATH.'/framework/class.secure.php');
+} else {
+   $oneback = "../";
+   $root = $oneback;
+   $level = 1;
+   while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+      $root .= $oneback;
+      $level += 1;
+   }
+   if (file_exists($root.'/framework/class.secure.php')) {
+      include($root.'/framework/class.secure.php');
+   } else {
+      trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+   }
+}
+// end include class.secure.php
+
 
 $root = LEPTON_PATH; ##rtrim($_SERVER['DOCUMENT_ROOT'],'/'); // don't touch this configuration
 
