36b4758a8df89d8e3e0a41f42f3dd89e9eceb776
labby <office@cms-lab.com>
2014-11-29 11:55:18 +0100

    prepare RC

--- upload/admins/interface/version.php
+++ upload/admins/interface/version.php
@@ -39,7 +39,7 @@ if (defined('LEPTON_PATH')) {
  *
  */
 if (!defined('CORE')) define('CORE', 'LEPTON');
-if (!defined('VERSION')) define('VERSION', '2.0.0 beta');
+if (!defined('VERSION')) define('VERSION', '2.0.0 RC');
 // for personal subversions only if needed
 if (!defined('SUBVERSION')) define('SUBVERSION', '');
 
