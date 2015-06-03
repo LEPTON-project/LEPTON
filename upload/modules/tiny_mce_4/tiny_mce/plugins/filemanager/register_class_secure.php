7e5d0932ed2a21077ab92dd5b3f3af3ca3035225
aldus <info@drp-x-work.de>
2015-06-03 12:09:43 +0200

    Bugfix inside TinyMCE filemanager upload.php (register class.secure).

--- upload/modules/tiny_mce_4/tiny_mce/plugins/filemanager/register_class_secure.php
+++ upload/modules/tiny_mce_4/tiny_mce/plugins/filemanager/register_class_secure.php
@@ -4,7 +4,7 @@
  *  @module         TinyMCE-4-jQ
  *  @version        see info.php of this module
  *  @authors        erpe, Dietrich Roland Pehlke (Aldus)
- *  @copyright      2012-2014 erpe, Dietrich Roland Pehlke (Aldus)
+ *  @copyright      2012-2015 erpe, Dietrich Roland Pehlke (Aldus)
  *  @license        GNU General Public License
  *  @license terms  see info.php of this module
  *  @platform       see info.php of this module
@@ -19,7 +19,8 @@ if (!is_object($lepton_filemanager)) require_once( "../../../../../framework/cla
 
 
 $files_to_register = array(
-	'/modules/tiny_mce_4/tiny_mce/plugins/filemanager/dialog.php'
+	'/modules/tiny_mce_4/tiny_mce/plugins/filemanager/dialog.php',
+	'/modules/tiny_mce_4/tiny_mce/plugins/filemanager/upload.php'
 );
 
 $lepton_filemanager->register( $files_to_register );
