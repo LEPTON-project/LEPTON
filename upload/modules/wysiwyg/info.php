0571f2997c1f5c9c8fcfc4ed3fa6127dd001c3d1
aldus <info@drp-x-work.de>
2015-07-01 12:20:37 +0200

    Update module version of wysiwyg to 3.1.5

--- upload/modules/wysiwyg/info.php
+++ upload/modules/wysiwyg/info.php
@@ -38,7 +38,7 @@ if (defined('LEPTON_PATH')) {
 $module_directory	= 'wysiwyg';
 $module_name		= 'WYSIWYG';
 $module_function	= 'page';
-$module_version		= '3.1.4';
+$module_version		= '3.1.5';
 $module_platform	= '2.x';
 $module_delete		=  false;
 $module_author		= 'Ryan Djurovich, Dietrich Roland Pehlke (last)';
