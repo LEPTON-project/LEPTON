127ab0b83ce64ab7c30fed5b71d5be367f291089
labby <rp@cms-lab.com>
2015-07-20 13:31:48 +0200

    added new function tom switch backend-theme

---
+++ upload/framework/functions/function.switch_theme.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of LEPTON Core, released under the GNU GPL
+ *
+ * @function		switch_theme
+ * @author          LEPTON Project
+ * @copyright       2010-2015 LEPTON Project
+ * @link            http://www.LEPTON-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see LICENSE and COPYING files in your package
+ *
+ */
+
+ /**
+ *	This file is included if the backend-theme setting has been modified
+ *
+ *	see admins/settings/save.php  around line 202
+ *	or
+ *	backend/settings/save.php  around line 202
+ *
+ */
+?>
