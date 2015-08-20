124155797b838f9e65c738e53b0799ae4c15a61d
labby <rp@cms-lab.com>
2015-08-20 09:53:55 +0200

    remove comment line

--- upload/admins/pages/index.php
+++ upload/admins/pages/index.php
@@ -39,9 +39,7 @@ require_once(LEPTON_PATH.'/framework/class.admin.php');
 $admin = new admin('Pages', 'pages');
 // Include the functions file
 
-// require_once(LEPTON_PATH.'/framework/functions.php');
 // eggsurplus: add child pages for a specific page
-
 
 // Setup template object
 $template = new Template(THEME_PATH.'/templates');
