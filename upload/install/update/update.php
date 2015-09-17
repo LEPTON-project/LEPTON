b8c0c7439c88fa0d859301dedfd09e38367254d7
labby <rp@cms-lab.com>
2015-09-17 11:20:01 +0200

    prepare next release

--- upload/install/update/update.php
+++ upload/install/update/update.php
@@ -62,6 +62,18 @@ if (!is_object($admin))
 		} else {
 		die ("<h3 class='good'>You don't have to update, you are running current LEPTON release.</h3>");
 		}
+		
+		/**
+		 *  update to LEPTON 2.1.1 , check release
+		 */
+		$lepton_version = $database->get_one("SELECT `value` from `" . TABLE_PREFIX . "settings` where `name`='lepton_version'");
+		if (version_compare($lepton_version, "2.1.0", "="))
+		{
+			echo("<h3 class='good'>Your LEPTON Version : $lepton_version </h3>");
+		    include 'scripts/211_update.php';
+		} else {
+		die ("<h3 class='good'>You don't have to update, you are running current LEPTON release.</h3>");
+		}		
 
 		/**
 		 *  reload all addons
