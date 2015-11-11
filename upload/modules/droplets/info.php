8555ba3ebef93a530f80d188974b52ed65b408f4
labby <rp@cms-lab.com>
2015-11-11 09:28:51 +0100

    bugfix in email filter

--- upload/modules/droplets/info.php
+++ upload/modules/droplets/info.php
@@ -43,7 +43,7 @@ else
 $module_directory = 'droplets';
 $module_name = 'Droplets';
 $module_function = 'tool';
-$module_version = '2.1.1';
+$module_version = '2.1.2';
 $module_platform = '2.x';
 $module_delete =  false;
 $module_author = 'LEPTON Project';
---
+++ upload/modules/droplets/js/mdcr.js
@@ -0,0 +1,40 @@
+/**
+ * This file is part of an ADDON for use with LEPTON Core.
+ * This ADDON is released under the GNU GPL.
+ * Additional license terms can be seen in the info.php of this module.
+ *
+ * @module          Droplets
+ * @author          LEPTON Project
+ * @copyright       2010-2015 LEPTON Project
+ * @link            http://www.LEPTON-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see info.php of this module
+ *
+ */
+// for droplet "EmailFilter"
+function mdcr(a,b) {
+  location.href=sdcr(a,b);
+}
+
+function sdcr(a,f) {
+  var b = a.charCodeAt(a.length-1) -97;
+  var c=""; var e; var g;
+  
+  for(var d=a.length-2; d>-1; d--) {
+    if(a.charCodeAt(d) < 97) {
+      switch(a.charCodeAt(d)) {
+        case 70: g=64; break;
+        case 90: g=46; break;
+        case 88: g=95; break;
+        case 75: g=45; break;
+        default: g=a.charCodeAt(d); break;
+      }
+      c+=String.fromCharCode(g)
+    } else {
+      e=(a.charCodeAt(d) - 97 - b) % 26;
+      e+=(e<0 || e>25) ? +26 : 0;
+      c+=String.fromCharCode(e+97);
+    }
+  }
+  return "mailto:"+c+f;
+}
