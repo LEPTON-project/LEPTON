6a21f7f599853af5686ad1e4432872757c323ae2
labby <rp@cms-lab.com>
2016-07-08 08:00:18 +0200

    update jquery to 3.1.0 http://blog.jquery.com/2016/07/07/jquery-3-1-0-released-no-more-silent-errors/

--- upload/modules/lib_jquery/info.php
+++ upload/modules/lib_jquery/info.php
@@ -38,7 +38,7 @@ if (defined('LEPTON_PATH')) {
 $module_directory = 'lib_jquery';
 $module_name      = 'jQuery Initial Library';
 $module_function  = 'library';
-$module_version   = '0.3.0.0';
+$module_version   = '0.3.1.0';
 $module_platform  = '1.x';
 $module_delete	  =  false;
 $module_author    = 'LEPTON Project';
