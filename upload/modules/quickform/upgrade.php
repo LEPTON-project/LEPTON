2e0d05197ef8ea9a7953a2d51006356e68ab5b80
labby <rp@cms-lab.com>
2016-07-11 16:15:19 +0200

    rename template files via upgrade.php

--- upload/modules/quickform/upgrade.php
+++ upload/modules/quickform/upgrade.php
@@ -64,6 +64,45 @@ $database->simple_query('CREATE TABLE IF NOT EXISTS `'.TABLE_PREFIX.'mod_quickfo
 	);
 
 // insert content from sik_table to original table
-$database->simple_query("INSERT INTO `".TABLE_PREFIX."mod_quickform_data` SELECT * FROM `".TABLE_PREFIX."xsik_quickform_data`");	  
+$database->simple_query("INSERT INTO `".TABLE_PREFIX."mod_quickform_data` SELECT * FROM `".TABLE_PREFIX."xsik_quickform_data`");
 
+//rename template file names to lower cases
+$path_de= __DIR__.'/templates/de/';
+if (file_exists($path_de.'contactform (HTML5).lte')) {
+	rename($path_de.'contactform (HTML5).lte',$path_de.'contactform_(html5).lte');
+}
+if (file_exists($path_de.'mini_contactform (HTML5).lte')) {
+	rename($path_de.'mini_contactform (HTML5).lte',$path_de.'mini_contactform_(html5).lte');
+}
+if (file_exists($path_de.'mini_contactform_semantic (HTML5).lte')) {
+	rename($path_de.'mini_contactform_semantic (HTML5).lte',$path_de.'mini_contactform_semantic_(html5).lte');
+}
+
+$path_en= __DIR__.'/templates/en/';
+if (file_exists($path_en.'callme_(HTML5).lte')) {
+	rename($path_en.'callme_(HTML5).lte',$path_en.'callme_(html5).lte');
+}
+if (file_exists($path_en.'contactform_(HTML5).lte')) {
+	rename($path_en.'contactform_(HTML5).lte',$path_en.'contactform_(html5).lte');
+}
+if (file_exists($path_en.'full_contactform_(HTML5).lte')) {
+	rename($path_en.'full_contactform_(HTML5).lte',$path_en.'full_contactform_(html5).lte');
+}
+if (file_exists($path_en.'mini_contactform_(HTML5).lte')) {
+	rename($path_en.'mini_contactform_(HTML5).lte',$path_en.'mini_contactform_(html5).lte');
+}
+
+$path_nl= __DIR__.'/templates/nl/';
+if (file_exists($path_nl.'bel_me_terug (HTML5).lte')) {
+	rename($path_nl.'bel_me_terug (HTML5).lte',$path_nl.'bel_me_terug_(html5).lte');
+}
+if (file_exists($path_nl.'contactformulier (HTML5).lte')) {
+	rename($path_nl.'contactformulier (HTML5).lte',$path_nl.'contactformulier_(html5).lte');
+}
+if (file_exists($path_nl.'mini_contactformulier (HTML5).lte')) {
+	rename($path_nl.'mini_contactformulier (HTML5).lte',$path_nl.'mini_contactformulier_(html5).lte');
+}
+if (file_exists($path_nl.'uitgebreid_contactformulier (HTML5).lte')) {
+	rename($path_nl.'uitgebreid_contactformulier (HTML5).lte',$path_nl.'uitgebreid_contactformulier_(html5).lte');
+}
 ?>
