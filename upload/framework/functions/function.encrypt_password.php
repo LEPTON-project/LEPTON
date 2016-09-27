9753db99e90a7ffde6d7cb4295aa7115e43c9f5e
aldus <info@drp-x-work.de>
2016-09-27 19:11:32 +0200

    Add missing file inside framework

---
+++ upload/framework/functions/function.encrypt_password.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * This file is part of LEPTON Core, released under the GNU GPL
+ *
+ * @function		encrypt_passwort
+ * @author          LEPTON Project
+ * @copyright       2010-2016 LEPTON Project
+ * @link            http://www.LEPTON-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see LICENSE and COPYING files in your package
+ *
+ */
+ 
+// include class.secure.php to protect this file and the whole CMS!
+if ( defined( 'LEPTON_PATH' ) )
+{
+	include( LEPTON_PATH . '/framework/class.secure.php' );
+} //defined( 'LEPTON_PATH' )
+else
+{
+	$oneback = "../";
+	$root    = $oneback;
+	$level   = 1;
+	while ( ( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) ) )
+	{
+		$root .= $oneback;
+		$level += 1;
+	} //( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) )
+	if ( file_exists( $root . '/framework/class.secure.php' ) )
+	{
+		include( $root . '/framework/class.secure.php' );
+	} //file_exists( $root . '/framework/class.secure.php' )
+	else
+	{
+		trigger_error( sprintf( "[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER[ 'SCRIPT_NAME' ] ), E_USER_ERROR );
+	}
+}
+// end include class.secure.php
+
+/**
+ *	First function to encrypt password
+ *	Encrypt given password with sha256 and LEPTON_GUID 
+ *	LEPTON_GUID ensures that password is related to this installation, even if the same password is used in other cases
+ *	Then salt hash.
+ *	
+ */
+function encrypt_password($password, $salt)
+{
+     return hash('sha256', $password . $salt);
+}
+
+?>
