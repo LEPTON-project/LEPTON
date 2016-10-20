77614d821683c70cd2eecba4f4d5be9a2951474e
aldus <info@drp-x-work.de>
2016-10-20 09:54:39 +0200

    Add core_function »get_leptoken«

---
+++ upload/framework/functions/function.get_leptoken.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of LEPTON Core, released under the GNU GPL
+ *
+ * @function		get_leptoken
+ * @author          LEPTON Project
+ * @copyright       2010-2016 LEPTON Project
+ * @link            http://www.LEPTON-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see LICENSE and COPYING files in your package
+ *
+ */
+
+if ( !defined( 'LEPTON_PATH' ) ) die();
+
+/**
+ *	Try to geht the current leptoken.
+ *
+ *	@param	int		Any root-(page) id. Default = 0.
+ *
+ */
+function get_leptoken() {
+
+	$leptoken = "";
+	if(isset($_POST['leptoken'])) {
+		$leptoken = $_POST['leptoken'];
+	} elseif (isset($_GET['leptoken'])) {
+		$leptoken = $_GET['leptoken'];
+	} elseif ( isset($_GET['amp;leptoken']) ) {
+		$leptoken = $_GET['amp;leptoken'];
+	}
+	
+	return $leptoken;
+}
+ 
