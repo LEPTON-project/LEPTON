de8d7515c784c2c49ce3856aabc9ad9d7eb37dd4
labby <rp@cms-lab.com>
2016-10-25 12:04:32 +0200

    prepare next release

--- upload/admins/interface/version.php
+++ upload/admins/interface/version.php
@@ -39,7 +39,7 @@ if (defined('LEPTON_PATH')) {
  *
  */
 if (!defined('CORE')) define('CORE', 'LEPTON');
-if (!defined('VERSION')) define('VERSION', '2.3.0');
+if (!defined('VERSION')) define('VERSION', '2.3.1');
 // for personal subversions only if needed
 if (!defined('SUBVERSION')) define('SUBVERSION', '');
 
--- upload/install/update/update.php
+++ upload/install/update/update.php
@@ -90,16 +90,6 @@ if (!is_object($admin))
 		    include 'scripts/222_update.php';
 		} 
 
-		/**
-		 *  update to LEPTON 2.3.0 , check release
-		 */		 
-		$lepton_version = $database->get_one("SELECT `value` from `" . TABLE_PREFIX . "settings` where `name`='lepton_version'");
-		if (version_compare($lepton_version, "2.2.2", "="))
-		{
-			echo("<h3 class='good'>Your LEPTON Version : $lepton_version </h3>");
-		} 	else {
-					die ("<h3 class='good'>You don't have to update, you are running current LEPTON release.</h3>");
-		}
 
 		/**
 		 *  check php version
@@ -109,9 +99,28 @@ if (!is_object($admin))
 			die ("<h3 class='bad'>No update possible, please update your PHP version to greater 5.3.7.<br />Your PHP Version : ". PHP_VERSION ." !</h3>");						
 		} 	else { 
 					echo("<h3 class='good'>Your PHP Version : ". PHP_VERSION ." !</h3>");
-					include 'scripts/230_update.php';
+					
+					$lepton_version = $database->get_one("SELECT `value` from `" . TABLE_PREFIX . "settings` where `name`='lepton_version'");
+					if (version_compare($lepton_version, "2.2.2", "="))
+						{
+							echo("<h3 class='good'>Your LEPTON Version : $lepton_version </h3>");
+							include 'scripts/230_update.php';
+						} 										
 		}
-		    		
+
+		/**
+		 *  update to LEPTON 2.3.1 , check release
+		 */		 
+		$lepton_version = $database->get_one("SELECT `value` from `" . TABLE_PREFIX . "settings` where `name`='lepton_version'");
+		if (version_compare($lepton_version, "2.3.0", "="))
+		{
+			echo("<h3 class='good'>Your LEPTON Version : $lepton_version </h3>");
+		    include 'scripts/231_update.php';
+			
+		} 	else {
+					die ("<h3 class='good'>You don't have to update, you are running current LEPTON release.</h3>");
+		}
+		
 		/**
 		 *  reload all addons
 		 */
