19a2a5bca108733b34f466eb610dce9c9327d9c9
aldus <drp@cms-lab.com>
2016-11-23 20:20:08 +0100

    Add some comments to the "new" classes

--- upload/framework/classes/lepton_tools.php
+++ upload/framework/classes/lepton_tools.php
@@ -17,7 +17,21 @@
  
 class LEPTON_tools
 {
-
+	/**
+	 *	Static method to return the result of a "printr" call for a given object/address.
+	 * 
+	 *	@param	mixed 	Any (e.g. mostly an object instance, or e.g. an array
+	 *	@param	string	Optional a "tag" (-name). Default is "pre".
+	 *	@param	string	Optional a class name for the tag.
+	 *
+	 *	E.g.
+	 *		LEPTON_tools::display( $result_array, "code", "example_class" )
+	 *
+	 *		will return e.g.
+	 *		<code class="example_class">
+	 *			array( [1] => "whatever");
+	 *		</code>
+	 */
 	static function display( $something_to_display ="", $tag="pre", $css_class=NULL ) {
 	
 		$s = "\n<".$tag.( NULL === $css_class ? "" : " class='".$css_class."'").">\n";
@@ -29,6 +43,23 @@ class LEPTON_tools
 		return $s;
 	}
 	
+	/**
+	 *	Static method to "require" a (LEPTON-) internal function file 
+	 *
+	 *	@param	string	A function name, and/or an array with function-names
+	 *
+	 *	e.g.
+	 *		//	one single function
+	 *		LEPTON_tools::register( "bind_jquery" );
+	 *
+	 *		//	a set of functions
+	 *		LEPTON_tools::register( "get_menu_title", "isValidMd5", "js_alert_encode" );
+	 *
+	 *		//	a set of function names inside an array
+	 *		$all_needed= array("get_menu_title", "isValidMd5", "js_alert_encode" );
+	 *		LEPTON_tools::register( $all_needed, "rm_full_dir" );
+	 *
+	 */
 	static function register() {
 		
 		if( 0 === func_num_args() ) return false;
@@ -48,6 +79,12 @@ class LEPTON_tools
 		}
 	}
 	
+	/**
+	 *	Try to "require" one or more local files.
+	 *
+	 *	@param	mixed	A (local) filepath, a set of paths and/or an array with paths
+	 *
+	 */
 	static function load() {
 		
 		if( 0 === func_num_args() ) return false;
@@ -66,6 +103,13 @@ class LEPTON_tools
 		}
 	}
 	
+	/**
+	 *	Returns the given leptoken hash
+	 *
+	 *	@param	none 
+	 *	@return string	The given Leptoken-Hash or an empty string
+	 *
+	 */
 	static function get_leptoken() {
 		$leptoken = "";
 		if(isset($_POST['leptoken'])) {
