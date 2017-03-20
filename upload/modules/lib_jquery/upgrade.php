9a84c7d5a616dca7149b3581487b6595343802a8
labby <rp@cms-lab.com>
2017-03-20 16:49:54 +0100

    delete old stuff

--- upload/modules/lib_jquery/upgrade.php
+++ upload/modules/lib_jquery/upgrade.php
@@ -40,6 +40,8 @@ else
 }
 // end include class.secure.php
 
+
+
 if (file_exists (LEPTON_PATH.'/modules/lib_jquery/jquery-ui/external/index.php')) {
 		rm_full_dir( LEPTON_PATH.'/modules/lib_jquery/jquery-ui/external' ); 
 }
@@ -48,6 +50,22 @@ if (file_exists (LEPTON_PATH.'/modules/lib_jquery/jquery-ui/external/index.php')
  *  delete not needed files
  */
 $temp_path = LEPTON_PATH."/modules/lib_jquery/jquery-core/1_jquery-core.min.js";
+if (file_exists($temp_path)) {
+	$result = unlink ($temp_path);
+	if (false === $result) {
+		echo "Cannot delete file ".$temp_path.". Please check file permissions and ownership or delete file manually.";
+	}
+}
+
+$temp_path = LEPTON_PATH."/modules/lib_jquery/jquery-core/2_jquery-core.min.js";
+if (file_exists($temp_path)) {
+	$result = unlink ($temp_path);
+	if (false === $result) {
+		echo "Cannot delete file ".$temp_path.". Please check file permissions and ownership or delete file manually.";
+	}
+}
+
+$temp_path = LEPTON_PATH."/modules/lib_jquery/jquery-core/2_jquery-migrate.min.js";
 if (file_exists($temp_path)) {
 	$result = unlink ($temp_path);
 	if (false === $result) {
