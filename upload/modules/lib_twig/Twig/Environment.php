3545f78135d5b05f8316e6342bb6fc9d7697a46d
labby <rp@cms-lab.com>
2017-04-26 16:58:27 +0200

    update twig to 2.3.2.0

--- upload/modules/lib_twig/Twig/Environment.php
+++ upload/modules/lib_twig/Twig/Environment.php
@@ -16,11 +16,11 @@
  */
 class Twig_Environment
 {
-    const VERSION = '2.3.1';
-    const VERSION_ID = 20301;
+    const VERSION = '2.3.2';
+    const VERSION_ID = 20302;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 3;
-    const RELEASE_VERSION = 1;
+    const RELEASE_VERSION = 2;
     const EXTRA_VERSION = '';
 
     private $charset;
