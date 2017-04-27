9fe51a7730b3d76921296400e1fb879b8a371032
labby <rp@cms-lab.com>
2017-04-27 11:35:21 +0200

    bugfix droplet edit this page

---
+++ upload/templates/lepton3/images/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *  @template       LEPTON-Start
+ *  @version        see info.php of this template
+ *  @author         cms-lab
+ * @copyright       2010-2017 CMS-LAB
+ *  @license        http://creativecommons.org/licenses/by/3.0/
+ *  @license terms  see info.php of this template
+ *  @platform       see info.php of this template
+ */
+
+// include class.secure.php to protect this file and the whole CMS!
+if ( defined( 'LEPTON_PATH' ) )
+{
+    include( LEPTON_PATH . '/framework/class.secure.php' );
+} 
+else
+{
+    $oneback = "../";
+    $root    = $oneback;
+    $level   = 1;
+    while ( ( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) ) )
+    {
+        $root .= $oneback;
+        $level += 1;
+    } 
+    if ( file_exists( $root . '/framework/class.secure.php' ) )
+    {
+        include( $root . '/framework/class.secure.php' );
+    } 
+    else
+    {
+        trigger_error( sprintf( "[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER[ 'SCRIPT_NAME' ] ), E_USER_ERROR );
+    }
+}
+// end include class.secure.php
+
+
+
+?>
