f0f751c85998b8ebe4532819edc3d0f78feae46e
labby <rp@cms-lab.com>
2017-07-27 11:16:59 +0200

    change release no.

--- upload/admins/interface/version.php
+++ upload/admins/interface/version.php
@@ -39,7 +39,7 @@ if (defined('LEPTON_PATH')) {
  *
  */
 if (!defined('CORE')) define('CORE', 'LEPTON');
-if (!defined('VERSION')) define('VERSION', '3.0.0-RC2');
+if (!defined('VERSION')) define('VERSION', '3.0.0');
 // for personal subversions only if needed
 if (!defined('SUBVERSION')) define('SUBVERSION', '');
 
