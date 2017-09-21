02e102d4ee43f4aa6b691a0c4d009e39fab719e7
labby <rp@cms-lab.com>
2017-09-21 12:28:40 +0200

    bugfix save css files

--- upload/modules/edit_module_files.php
+++ upload/modules/edit_module_files.php
@@ -42,17 +42,12 @@ if(!(isset($_POST['page_id']) && isset($_POST['section_id']) && isset($_POST['ac
 // include the and admin wrapper script
 require(LEPTON_PATH.'/modules/admin.php');
 
-// leave if the required module.functions.php file does not exist
-if(!file_exists(LEPTON_PATH .'/framework/summary.module_edit_css.php')) {
-	echo 'The required file: /framework/summary.module_edit_css.php is missing - script stopped.';
-	die;
-}
 
 // Include EditArea wrapper functions
 require_once(LEPTON_PATH . '/modules/edit_area/register.php');
 echo (function_exists('registerEditArea')) ? registerEditArea('code_area', 'css', false) : 'none';
 
-// set default text output if varibles are not defined in the global WB language files
+// set default text output if varibles are not defined in the global language files
 $HEADING_CSS_FILE = (isset($GLOBALS['TEXT']['HEADING_CSS_FILE'])) ?$GLOBALS['TEXT']['HEADING_CSS_FILE'] :'Actual module file: ';
 $TXT_EDIT_CSS_FILE = (isset($GLOBALS['TEXT']['TXT_EDIT_CSS_FILE'])) ?$GLOBALS['TEXT']['TXT_EDIT_CSS_FILE'] :'Edit the CSS definitions in the textarea below.';
 
@@ -62,48 +57,12 @@ require_once(LEPTON_PATH .'/framework/summary.module_edit_css.php');
 // check if the module directory is valid
 $mod_dir = $_POST['mod_dir'];
 
-// check if action is: save or edit
-if($_POST['action'] == 'save' && mod_file_exists($mod_dir, $_POST['edit_file'])) {
-	/** 
-		SAVE THE UPDATED CONTENTS TO THE CSS FILE
-	*/
-	$css_content = '';
-	if (isset($_POST['css_data']) && strlen($_POST['css_data']) > 0) {
-		$css_content = stripslashes($_POST['css_data']);
-	}
-
-	$bytes = 0;
-	if ($css_content != '')
-    {
-		// open the module CSS file for writting
-		$mod_file = fopen(LEPTON_PATH .'/modules/' .$mod_dir .'/' .$_POST['edit_file'], 'wb');
-		// write new content to the module CSS file
-		$bytes = fwrite($mod_file, $css_content);
-		// close the file
-		fclose($mod_file);
-	}
-
-	// write out status message
-	if($bytes == 0 )
-    {
-		$admin->print_error($TEXT['ERROR'], ADMIN_URL.'/pages/modify.php?page_id='.$page_id);
-	} else {
-		$admin->print_success($TEXT['SUCCESS'], ADMIN_URL.'/pages/modify.php?page_id='.$page_id);
-	}
-
-
-} else {
+if($_POST['action'] != 'save') 
+{
 
 	/** 
-		MODIFY CONTENTS OF THE CSS FILE VIA TEXT AREA 
+		DISPLAY CONTENTS OF THE CSS FILE VIA TEXT AREA 
 	*/
-	// check if module backend.css file needs to be included into the <body>
-	if((!method_exists($admin, 'register_backend_modfiles') || !isset($_GET['page_id']))
-			&& file_exists(LEPTON_PATH .'/modules/'.$mod_dir.'/backend.css')) {
-		echo '<style type="text/css">';
-		include(LEPTON_PATH .'/modules/' .$mod_dir .'/backend.css');
-		echo "\n</style>\n";
-	}
 
 	// check which module file to edit (frontend.css, backend.css or '')
 	$css_file = (in_array($_POST['edit_file'], array('frontend.css', 'backend.css'))) ? $_POST['edit_file'] : '';
@@ -161,7 +120,7 @@ if($_POST['action'] == 'save' && mod_file_exists($mod_dir, $_POST['edit_file']))
 	  	<input type="hidden" name="page_id" value="<?php echo $page_id; ?>" />
 	  	<input type="hidden" name="section_id" value="<?php echo $section_id; ?>" />
 	  	<input type="hidden" name="mod_dir" value="<?php echo $mod_dir; ?>" />
-		<input type="hidden" name="edit_file" value="<?php echo $css_file; ?>" />
+		<input type="hidden" name="edit_file" value="<?php echo LEPTON_PATH.$path.$css_file; ?>" />
 	  	<input type="hidden" name="action" value="save" />
 		<input type="hidden" name="path" value="<?php echo $path ?>" />
 		<textarea id="code_area" name="css_data" cols="115" rows="25" wrap="VIRTUAL" style="margin:2px;width:100%;"><?php
@@ -185,6 +144,37 @@ if($_POST['action'] == 'save' && mod_file_exists($mod_dir, $_POST['edit_file']))
 		<?php 
 	}
 }
+// check if action is: save or edit
+elseif($_POST['action'] == 'save') {
+	/** 
+		SAVE THE UPDATED CONTENTS TO THE CSS FILE
+	*/
+	$css_content = '';
+	if (isset($_POST['css_data']) && strlen($_POST['css_data']) > 0) {
+		$css_content = stripslashes($_POST['css_data']);
+	}
+
+	if ($css_content != '')
+    {
+		// open the module CSS file for writting
+		$mod_file = fopen($_POST['edit_file'], 'wb');
+		// write new content to the module CSS file
+		$bytes_written = fwrite($mod_file, $css_content);
+		// close the file
+		fclose($mod_file);
+	}
+
+	// write out status message
+	if($bytes_written == false )
+    {
+		$admin->print_error($TEXT['ERROR'], ADMIN_URL.'/pages/modify.php?page_id='.$page_id);
+	} else {
+		$admin->print_success($TEXT['SUCCESS'], ADMIN_URL.'/pages/modify.php?page_id='.$page_id);
+	}
+
+
+} 
+
 
 // Print admin footer
 $admin->print_footer();
