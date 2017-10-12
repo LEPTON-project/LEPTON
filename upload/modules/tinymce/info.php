490d3d0d5b595e8d5d4f96e04b2579f069f196ab
labby <rp@cms-lab.com>
2017-10-12 14:33:08 +0200

    update tinymce to 4.7.1.0

--- upload/modules/tinymce/info.php
+++ upload/modules/tinymce/info.php
@@ -35,7 +35,7 @@ if (defined('LEPTON_PATH')) {
 $module_directory     = 'tinymce';
 $module_name          = 'TinyMCE';
 $module_function      = 'WYSIWYG';
-$module_version       = '4.7.0.0';
+$module_version       = '4.7.1.0';
 $module_platform      = '3.x';
 $module_author        = 'erpe, Aldus';
 $module_home          = 'http://lepton-cms.org';
