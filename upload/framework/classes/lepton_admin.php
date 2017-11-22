76297426cb480819ccc5842ae7ab0286f3a920ee
labby <rp@cms-lab.com>
2017-11-22 18:10:22 +0100

    small correction

--- upload/framework/classes/lepton_admin.php
+++ upload/framework/classes/lepton_admin.php
@@ -447,7 +447,7 @@ class LEPTON_admin extends LEPTON_core
     public function print_footer()
     {
         $footer_vars = array(
-            'BACKEND_BODY_MODULE_JS' => get_page_footers('backend', false),			
+            'BACKEND_BODY_MODULE_JS' => get_page_footers('backend'),			
             'LEPTON_URL' => LEPTON_URL,
             'LEPTON_PATH' => LEPTON_PATH,
             'ADMIN_URL' => ADMIN_URL,
