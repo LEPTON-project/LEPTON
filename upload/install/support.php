dc96523b11b6f158e9e83a87a96b4112c825969c
labby <rp@cms-lab.com>
2017-12-19 16:34:59 +0100

    typo

--- upload/install/support.php
+++ upload/install/support.php
@@ -61,8 +61,8 @@
 
 		<h3><a class='update_link1' href=' https://www.lepton-cms.org/english/contact.php' target='_blank'><h3>or support LEPTON in another way</a> </h3><br />
 
-		<?php LEPTON_handle::delete_obsolete_directories('/install'); ?
-		> 		
+		<?php LEPTON_handle::delete_obsolete_directories('/install'); ?>
+ 		
 		<h3><a class='update_link2' href="<?php echo ADMIN_URL; ?>/login/index.php">please login and check installation</></h3>
 
 		<div class="spacer"></div>		
