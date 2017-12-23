90b4bcd04d10f66cd040aa8da7f912504d28bff7
labby <rp@cms-lab.com>
2017-12-23 16:14:14 +0100

    replace button and footer

--- upload/install/support.php
+++ upload/install/support.php
@@ -50,39 +50,14 @@
 		<div class="spacer"></div>		
 	</div>	
 
-	<div class="ui attached segment">
-		<div class="spacer"></div>
-		<h4 class="ui header">Please consider a donation to LEPTON</h4>
-		<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input name="cmd" type="hidden" value="_s-xclick" /> 
-			<input name="hosted_button_id" type="hidden" value="DF6TFNAE7F7DJ" /> 
-			<input alt="PayPal &mdash; The safer, easier way to donate online." name="submit" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" type="image" /> 
-			<img src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" border="0" alt="" width="1" height="1" />
-		</form>		
-		<div class="spacer"></div>	
-		<div class="spacer"></div>	
-		<div class="spacer"></div>			
-		<div class="column">
-			<div class="ui buttons">
-				<a href='https://www.lepton-cms.org/english/contact.php' target='_blank'><button class="ui orange button">support LEPTON</button></a>
-				<div class="or" data-text=" and "> </div>
-				<a href="<?php echo ADMIN_URL; ?>/login/index.php"><button class="ui positive button">login to check installation</button></a>
-			</div>
-		</div>			
+	<?php
+	// get the buttons					
+		include('update/login.php');
+		
+	// get the footer				
+		include('update/footer.php');		
+	?>
 
-		<div class="spacer"></div>		
-	</div>
-
-	<div class="ui bottom attached center alligned segment">
-		<div class="ui icon message lepton_footer">
-			<div class="content">
-			<!-- Please note: the below reference to the GNU GPL should not be removed, as it provides a link for users to read about warranty, etc. -->
-			<a href="https://lepton-cms.org" title="LEPTON CMS">LEPTON Core</a> is released under the
-			<a href="http://www.gnu.org/licenses/gpl.html" title="LEPTON Core is GPL">GNU General Public License</a>.
-			<!-- Please note: the above reference to the GNU GPL should not be removed, as it provides a link for users to read about warranty, etc. -->
-			<br /><a href="https://lepton-cms.org" title="LEPTON CMS">LEPTON CMS Package</a> is released under several different licenses.
-			</div>
-		</div>
-	</div>	
 	
 </div> <!-- end id="update_form" -->
 </body>
--- upload/install/update/update.php
+++ upload/install/update/update.php
@@ -74,36 +74,16 @@ if (!is_object($admin))
 		} 	else {
 					echo ("<h3 class='good'>You don't have to update, you are running current LEPTON release.</h3>");
 						// delete install directory
-						if ( file_exists(LEPTON_PATH.'/install/')) {
-							require_once (LEPTON_PATH.'/framework/functions/function.rm_full_dir.php');
-							rm_full_dir(LEPTON_PATH.'/install/');
-						} 						
+						
 					echo ("<div class='ui compact info message'><i class='big announcement icon'></i>Your install directory has been deleted!</div>");
 					
 					// return to installation
-					?>
-					<div class="ui attached segment">
-						<div class="spacer"></div>
-						<h4 class="ui header">Please consider a donation to LEPTON</h4>
-						<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input name="cmd" type="hidden" value="_s-xclick" /> 
-							<input name="hosted_button_id" type="hidden" value="DF6TFNAE7F7DJ" /> 
-							<input alt="PayPal &mdash; The safer, easier way to donate online." name="submit" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" type="image" /> 
-							<img src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" border="0" alt="" width="1" height="1" />
-						</form>		
-						<div class="spacer"></div>	
-						<div class="spacer"></div>	
-						<div class="spacer"></div>			
-						<div class="column">
-							<div class="ui buttons">
-								<a href='https://www.lepton-cms.org/english/contact.php' target='_blank'><button class="ui orange button">support LEPTON</button></a>
-								<div class="or" data-text=" and "> </div>
-								<a href="<?php echo ADMIN_URL; ?>/login/index.php"><button class="ui positive button">login to check installation</button></a>
-							</div>
-						</div>			
-
-						<div class="spacer"></div>		
-					</div>					
-					<?php
+					if ( file_exists(LEPTON_PATH.'/install/')) {
+						require_once (LEPTON_PATH.'/framework/functions/function.rm_full_dir.php');
+						rm_full_dir(LEPTON_PATH.'/install/');
+					}
+					// get the buttons					
+					include('login.php');	
 					die();
 		}		
 		/**
@@ -121,48 +101,17 @@ if (!is_object($admin))
 		?>				
 		<div class="spacer"></div>		
 	</div>
-		<?php
-			/**
-			 * delete install directory
-			 */
-			if ( file_exists(LEPTON_PATH.'/install/')) {
-				require_once (LEPTON_PATH.'/framework/functions/function.rm_full_dir.php');
-				rm_full_dir(LEPTON_PATH.'/install/');
-			} 	
-		?>	
-	<div class="ui attached segment">
-		<div class="spacer"></div>
-		<h4 class="ui header">Please consider a donation to LEPTON</h4>
-		<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input name="cmd" type="hidden" value="_s-xclick" /> 
-			<input name="hosted_button_id" type="hidden" value="DF6TFNAE7F7DJ" /> 
-			<input alt="PayPal &mdash; The safer, easier way to donate online." name="submit" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" type="image" /> 
-			<img src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" border="0" alt="" width="1" height="1" />
-		</form>		
-		<div class="spacer"></div>	
-		<div class="spacer"></div>	
-		<div class="spacer"></div>			
-		<div class="column">
-			<div class="ui buttons">
-				<a href='https://www.lepton-cms.org/english/contact.php' target='_blank'><button class="ui orange button">support LEPTON</button></a>
-				<div class="or" data-text=" and "> </div>
-				<a href="<?php echo ADMIN_URL; ?>/login/index.php"><button class="ui positive button">login to check installation</button></a>
-			</div>
-		</div>			
-
-		<div class="spacer"></div>		
-	</div>
-	
-	<div class="ui bottom attached center alligned segment">
-		<div class="ui icon message lepton_footer">
-			<div class="content">
-			<!-- Please note: the below reference to the GNU GPL should not be removed, as it provides a link for users to read about warranty, etc. -->
-			<a href="https://lepton-cms.org" title="LEPTON CMS">LEPTON Core</a> is released under the
-			<a href="http://www.gnu.org/licenses/gpl.html" title="LEPTON Core is GPL">GNU General Public License</a>.
-			<!-- Please note: the above reference to the GNU GPL should not be removed, as it provides a link for users to read about warranty, etc. -->
-			<br /><a href="https://lepton-cms.org" title="LEPTON CMS">LEPTON CMS Package</a> is released under several different licenses.
-			</div>
-		</div>
-	</div>	
+	<?php
+	// delete install directory
+	if ( file_exists(LEPTON_PATH.'/install/')) {
+		require_once (LEPTON_PATH.'/framework/functions/function.rm_full_dir.php');
+		rm_full_dir(LEPTON_PATH.'/install/');
+	} 
+	// get the buttons					
+	include('login.php');
+	// get the footer				
+	include('footer.php');		
+	?>	
 	
 </div> <!-- end id="update_form" -->
 </body>
