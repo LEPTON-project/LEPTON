000463d591e77279907f5c7ce33d89cf668940c6
labby <rp@cms-lab.com>
2018-01-30 17:07:52 +0100

    add info message to pdo

--- upload/framework/classes/lepton_database.php
+++ upload/framework/classes/lepton_database.php
@@ -174,7 +174,10 @@ class LEPTON_database
         		if(!defined("TABLE_PREFIX")) define("TABLE_PREFIX", $config['database']['prefix']);
         	
         		unset($config);
-        	}
+        	} else {
+                // Problem: no lepton.ini.php 
+                exit('<p><b>Sorry, but this installation seems to be damaged! Please contact your webmaster!</b></p>');
+			}
         }
         
         $setup = array(
