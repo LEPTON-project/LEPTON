2a16670514573717d0ba985c6a541bbe35d59ba9
aldus <aldus@venedig1501.de>
2018-02-07 18:56:21 +0100

    Codechanges inside backend/pages/restore

--- upload/backend/pages/restore.php
+++ upload/backend/pages/restore.php
@@ -36,8 +36,6 @@ if (defined('LEPTON_PATH')) {
 }
 // end include class.secure.php
 
-
-
 // Get page id
 if(!isset($_GET['page_id']) OR !is_numeric($_GET['page_id'])) {
 	header("Location: index.php");
@@ -46,66 +44,88 @@ if(!isset($_GET['page_id']) OR !is_numeric($_GET['page_id'])) {
 	$page_id = $_GET['page_id'];
 }
 
-
 $admin = new LEPTON_admin('Pages', 'pages_delete');
 
 // Include the functions file
 require_once(LEPTON_PATH.'/framework/summary.functions.php');
 
 // Get perms
-$results = $database->query("SELECT admin_groups,admin_users FROM ".TABLE_PREFIX."pages WHERE page_id = '$page_id'");
-$results_array = $results->fetchRow();
+// $results = $database->query("SELECT admin_groups,admin_users FROM ".TABLE_PREFIX."pages WHERE page_id = '$page_id'");
+// $results_array = $results->fetchRow();
 
 // Find out more about the page
-$query = "SELECT * FROM ".TABLE_PREFIX."pages WHERE page_id = '$page_id'";
-$results = $database->query($query);
-if($database->is_error()) {
+$aPageInfo = array();
+
+$database->execute_query(
+    "SELECT * FROM `".TABLE_PREFIX."pages` WHERE `page_id` = ".$page_id,
+    true,
+    $aPageInfo,
+    false
+);
+
+if($database->is_error())
+{
 	$admin->print_error($database->get_error());
 }
-if($results->numRows() == 0) {
+elseif( count($aPageInfo) == 0)
+{
 	$admin->print_error($MESSAGE['PAGES_NOT_FOUND']);
 }
-$results_array = $results->fetchRow();
-$old_admin_groups = explode(',', str_replace('_', '', $results_array['admin_groups']));
-$old_admin_users = explode(',', str_replace('_', '', $results_array['admin_users']));
+
+$old_admin_groups = explode(',', str_replace('_', '', $aPageInfo['admin_groups']));
+$old_admin_users = explode(',', str_replace('_', '', $aPageInfo['admin_users']));
 
 $in_old_group = FALSE;
-foreach($admin->get_groups_id() as $cur_gid){
-    if (in_array($cur_gid, $old_admin_groups)) {
-	$in_old_group = TRUE;
+foreach($admin->get_groups_id() as $cur_gid)
+{
+    if (in_array($cur_gid, $old_admin_groups))
+    {
+        $in_old_group = TRUE;
     }
 }
-if((!$in_old_group) AND !is_numeric(array_search($admin->get_user_id(), $old_admin_users))) {
-	$admin->print_error($MESSAGE['PAGES_INSUFFICIENT_PERMISSIONS']);
+
+if((!$in_old_group) AND !is_numeric(array_search($admin->get_user_id(), $old_admin_users)))
+{
+    $admin->print_error($MESSAGE['PAGES_INSUFFICIENT_PERMISSIONS']);
 }
 
-$visibility = $results_array['visibility'];
+/**
+ *  [1]
+ *  Function to change all child pages visibility to deleted
+ */
+function restore_subs($parent = 0) {
+    // global $database;
+    
+    // Query pages
+	$aSubpages = array();
+	$database->execute_query(
+	    "SELECT `page_id` FROM `".TABLE_PREFIX."pages` WHERE `parent` = '".$parent."' ORDER BY position ASC",
+	    true,
+	    $aSubpages,
+	    true;
+	);
 
-if(PAGE_TRASH) {
-	if($visibility == 'deleted') {	
-		// Function to change all child pages visibility to deleted
-		function restore_subs($parent = 0) {
-			global $database;
-			// Query pages
-			$query_menu = $database->query("SELECT page_id FROM ".TABLE_PREFIX."pages WHERE parent = '$parent' ORDER BY position ASC");
-			// Check if there are any pages to show
-			if($query_menu->numRows() > 0) {
-				// Loop through pages
-				while($page = $query_menu->fetchRow()) {
-					// Update the page visibility to 'deleted'
-					$database->query("UPDATE ".TABLE_PREFIX."pages SET visibility = 'public' WHERE page_id = '".$page['page_id']."' LIMIT 1");
-					// Run this function again for all sub-pages
-					restore_subs($page['page_id']);
-				}
-			}
-		}
+    foreach($aSubpages as $page)
+    {
+        // Update the page visibility to 'deleted'
+        $database->simple_query("UPDATE `".TABLE_PREFIX."pages` SET `visibility` = 'public' WHERE page_id = '".$page['page_id']."'");
+        
+        // Run function for all sub-pages ... See [1]
+        restore_subs($page['page_id']);
+    }
+}
 		
-		// Update the page visibility to 'deleted'
-		$database->query("UPDATE ".TABLE_PREFIX."pages SET visibility = 'public' WHERE page_id = '$page_id.' LIMIT 1");
+$visibility = $aPageInfo['visibility'];
+
+if(PAGE_TRASH)
+{
+	if($visibility == 'deleted')
+	{	
+        // Update the page visibility to 'public'
+        $database->simple_query("UPDATE `".TABLE_PREFIX."pages` SET `visibility` = 'public' WHERE page_id = ".$page_id);
 		
-		// Run trash subs for this page
-		restore_subs($page_id);
-		
+        // Run trash subs for this page
+        restore_subs($page_id);
 	}
 }
 
