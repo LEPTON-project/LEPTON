21f116e9b0bd93d5aede825456bc4a1fcf65e5fc
aldus <drp@cms-lab.com>
2018-02-20 15:26:28 +0100

    remove old code in FR (core language)

--- upload/languages/FR.php
+++ upload/languages/FR.php
@@ -711,13 +711,4 @@ $OVERVIEW = array(
 	"VIEW" 					=> "Aperçu du site dans une nouvelle fenêtre..."
 );
 
-/*
- * Create the old languages definitions only if specified in settings
- */
-if (ENABLE_OLD_LANGUAGE_DEFINITIONS) {
-	foreach ($MESSAGE as $key => $value) {
-		$x = strpos($key, "_");
-		$MESSAGE[substr($key, 0, $x)][substr($key, $x+1)] = $value;
-	}
-}
 ?>
