f1874814b3af7e06396d0e711b33b332cc510770
labby <rp@cms-lab.com>
2018-03-07 10:25:29 +0100

    change requirements

--- upload/modules/wrapper/info.php
+++ upload/modules/wrapper/info.php
@@ -46,23 +46,10 @@ $module_directory	= 'wrapper';
 $module_name		= 'Wrapper';
 $module_function	= 'page';
 $module_version		= '2.9.1';
-$module_platform	= '2.x';
+$module_platform	= '4.x';
 $module_author		= 'Ryan Djurovich, Dietrich Roland Pehlke (last)';
 $module_license		= 'GNU General Public License';
 $module_description	= 'This module allows you to wrap your site around another using an inline frame.';
 $module_guid		= 'a5830654-06f3-402a-9d25-a03c53fc5574';
 
-/**
- *	2.8.0	2014-09-06	- delete columns from table.
- *						- Try to set all db calls consequent to PDO.
- *						- LEPTON CMS 2.0 series strict!
- *  
- *	2.7.2.1	2012-03-01	- added columns to table.
- *						  
- *	2.7.1	2010-11-02	- Bugfix inside the html-template to get valid output.
- *						  (Remove missplaced '</form>' closing tag)
- *						- Move the html-template inside the "htt" folder.
- *						- Remove WB 2.7 support.
- *
- */
 ?>
